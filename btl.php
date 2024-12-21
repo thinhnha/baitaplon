1fec11b2fa74a56a25285f485caed9bd4f33f6b4
Huy180105 <huy74dcht21108@st.utt.edu.vn>
2024-12-22 00:32:46 +0700

    sửa lại code

--- btl.php
+++ btl.php
@@ -7,6 +7,12 @@ if(isset($_POST['search']))
     $sql = "SELECT * FROM sanpham WHERE name LIKE '%$nd%'";
 }
 $result = $conn->query($sql);
+// Kiểm tra nếu người dùng đã đăng nhập
+if (isset($_SESSION['user'])) {
+    $username = htmlspecialchars($_SESSION['user']);
+} else {
+    $username = "";
+}
 ?>
 
 <!DOCTYPE html>
@@ -14,75 +20,51 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="Css/app.css">
-    <title>Cửa hàng game giá rẻ</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"> <!-- Font Awesome -->
+    <link rel="stylesheet" href="Css/app.css"> <!-- Đường dẫn tới file CSS -->
+    <title>Trang chủ - Cửa hàng game giá rẻ</title>
 </head>
 <body>
-    <div id="wrapper">
-        <div id="header">
-            <nav class="container-menu">
-                <div class="head">
-                <a href="" id="logomenu">
-                    <img src="img/logomenu.png" alt="logomenu">
-                </a>
-                <div class="form-search">
-                    <form method="post">
-                  <input class="search" name="noidung" type="text" placeholder="Nhập thông tin để tìm kiếm">
-                  <button class="search-btn" type="submit" name="search"><i class="fa-solid fa-magnifying-glass"></i></button>
-                    </form>
-                </div>
-                </div>
-                <ul id="main-menu">
-                    <li><a href="">Trang chủ</a></li>
-                    <li><a href="">Đĩa</a>
-                        <ul class="sub-menu">
-                            <li><a href="">Game PS4</a></li>
-                            <li><a href="">Game PS5</a></li>
-                            <li><a href="">Game Nintendo Switch</a></li>
-                            <li><a href="">Game Xbox One</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="">Danh sách khách hàng</a></li>
-                    <li><a href="">Doanh thu</a></li>
-                </ul>
-                <div class="form-login">
-                    <button class="login-btn" type="button" data-toggle="modal" data-target="#myModal"> Đăng nhập </button>
-                </div>
-            </nav>
-        </div>
-        <div id="main-content">
-            <div id="sidebar">
-                <h2>Danh mục</h2>
-                <ul>
-                    <li><a href="">Html</a></li>
-                    <li><a href="">Css</a></li>
-                    <li><a href="">Js</a></li>
-                </ul>
-            </div>
-            <div id="picture">
-                <img src="img/picture.webp" alt="top 10 game" width="960px">
-            </div>
-            <div id="content">
-            <?php 
-        while($row = $result->fetch_assoc())
-        {$price = number_format($row['price'], 0, ',', '.') . " ₫";
-        echo "
-            <a href='' >
-            <img src='".$row['img']."' alt='sample picture' width='100px' height ='100px'>
-            <p>".$row['name']." </p>
-            <p>".$price."</p>
+<div id="header">
+    <nav class="container-menu">
+        <div class="head">
+            <a href="index.php" id="logomenu">
+                <img src="img/logomenu.png" alt="logomenu">
             </a>
-            ";
-            
-        }
-        ?>
-                
+            <!-- Thanh tìm kiếm -->
+            <div class="form-search">
+                <form method="POST" action="search.php">
+                    <input class="search" name="nd" type="text" placeholder="Nhập thông tin để tìm kiếm">
+                    <button class="search-btn" type="submit" name="btn">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
             </div>
         </div>
-        <div id="footer">
-            <p>Phần thông tin cuối</p>
-        </div>      
-    </div>
+        <ul id="main-menu">
+            <li><a href="index.php">Trang chủ</a></li>
+            <li><a href="#">Đĩa</a>
+                <ul class="sub-menu">
+                    <li><a href="#">Game Ps4</a></li>
+                    <li><a href="#">Game Ps5</a></li>
+                    <li><a href="#">Game Nintendo Switch</a></li>
+                    <li><a href="#">Game Xbox One</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Danh sách khách hàng</a></li>
+            <li><a href="#">Doanh thu</a></li>
+        </ul>
+        
+        <!-- Kiểm tra nếu người dùng đã đăng nhập -->
+        <div class="form-login">
+            <?php if (!empty($username)): ?>
+                <span class="user-info">Chào, <?php echo $username; ?>!</span>
+                <a href="logout.php" class="logout-btn">Đăng xuất</a>
+            <?php else: ?>
+                <button type="button" class="login-btn" onclick="window.location.href='login.php';">Đăng nhập</button>
+            <?php endif; ?>
+        </div>
+    </nav>
+</div>
 </body>
 </html>
