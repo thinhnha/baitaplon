80e147083d9f83d3f258c282f5b950aa21f8e915
Nguyễn Nam Khánh <nammkhanhnguyen@gmail.com>
2024-12-23 01:48:46 +0700

    sql

--- quanly.sql
+++ quanly.sql
@@ -58,6 +58,7 @@ CREATE TABLE `sanpham` (
   `name` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
   `type` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
   `img` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
