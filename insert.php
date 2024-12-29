be8e5a19dbcbc7645ed302d5cc1dc6df73501cff
Nguyễn Nam Khánh <nammkhanhnguyen@gmail.com>
2024-12-30 01:16:23 +0700

    them san pham

--- insert.php
+++ insert.php
@@ -1,12 +1,17 @@
 <?php
-require ('connect.php');
-if(isset($_POST['add']))
-{
+require('connect.php');
+$id = (int) $_GET['id'];
+$sql = "SELECT * FROM `sanpham` WHERE `id` = {$id}";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+if($_SERVER['REQUEST_METHOD']==='POST')
+{  
     $name = $_POST['name'];
     $price = intval($_POST['price']);
     $type = $_POST['type'];
     $quantity = $_POST['quantity'];
     $pname = $_FILES["img"]["name"];
+    $info = $_POST['info'];
     $upload_dir = 'images/';
     $upload_file= $upload_dir . basename($_FILES["img"]["name"]);
     $imageFileType = strtolower(pathinfo($upload_file,PATHINFO_EXTENSION));
@@ -17,61 +22,55 @@ if(isset($_POST['add']))
 }
   else { 
     move_uploaded_file($_FILES["img"]["tmp_name"], $upload_file); 
-    $sql = "INSERT INTO `sanpham` (`name`, `price`, `type`,`quantity`, `img`)
-            VALUES ('$name', '$price', '$type', '$quantity', '$upload_file')";
-    if($conn->query($sql) === TRUE )
-             {
-                 $message = "Thêm sản phẩm thành công";
-                 echo "<script type='text/javascript'>alert('$message');</script>";
-             }
-  }
-   
+    $update = "UPDATE sanpham SET name = '$name', price = '$price', type = '$type', quantity = '$quantity', img = '$upload_file', info = '$info' WHERE id = {$id}";
+if($conn ->query($update)===TRUE)
+{
+    header('Location:index.php');
 }
-
-$sql1 = "SELECT * FROM sanpham";
-$result = $conn->query($sql1);
+}
+}
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Nhập thông tin</title>
+
     </head>
     <body>
-        <div>
+    <div>
             <form enctype="multipart/form-data" method="post">
                 <div>
                 <label for="name">Tên sản phẩm:</label>
-                <input type="text" name="name" id="name">
+                <input type="text" name="name" id="name" value="<?=$row['name']?>">
                 </div>
                 <div>
                     <label for="price">Giá: </label>
-                    <input type="number" name="price" id="price">
+                    <input type="number" name="price" id="price" value="<?=$row['price']?>">
 </div>
                 <div>
                     <label for="">Thể loại:</label>
                     <select id="type" name="type">
-                    <option value="0">PS5</option>
-                    <option value="1">PS4</option>
-                    <option value="2">Nintendo Switch</option>
-                    <option value="3">XBOX ONE</option>
+                    <option value="0" <?= $row['type']==0 ? 'selected':''?>>PS5</option>
+                    <option value="1" <?= $row['type']==1 ? 'selected':''?>>PS4</option>
+                    <option value="2" <?= $row['type']==2 ? 'selected':''?>>Nintendo Switch</option>
+                    <option value="3" <?= $row['type']==3 ? 'selected':''?>>XBOX ONE</option>
                     </select>
                 </div>
                 <div>
                     <label for="quantity">Số lượng: </label>
-                    <input type="number" name="quantity" id="quantity">
+                    <input type="number" name="quantity" id="quantity" value="<?=$row['quantity']?>">
                 </div>
             <div>
                 <label for="img">Hình ảnh</label>
-                <input type="File" name="img" id="img">
+                <input type="File" name="img" id="img" value="<?=$row['img']?>">
             </div>  
             <div>
                 <label for="img">Thông tin sản phẩm: </label>
-                <textarea name="info" id="info"></textarea>
+                <textarea name="info" id="info" value="<?=$row['info']?>"></textarea>
             </div>
-                <input type="submit" name="add" value="Lưu">
+                <input type="submit" value="Lưu">
             </form>
         </div>
         <div>
         </div>
-</body>
+    </body>
 </html>
